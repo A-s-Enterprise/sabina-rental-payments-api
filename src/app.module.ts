d92e4cfea79438ff1f5f47a9eaef09b04dc27f21
Mark <mac21macky@gmail.com>
2022-11-10 09:26:36 +0800

    Apply new module and middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,26 @@
-import { Module } from '@nestjs/common';
-import { S3Module } from './s3/s3.module';
-import { ConfigurationModule } from './config/config.module';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { ScheduleModule } from '@nestjs/schedule';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MethodNotAllowedMiddleware } from './common/middlewares/MethodNotAllowedMiddleware';
 import { PrismaService } from './common/prisma.service';
+import { ConfigurationModule } from './config/config.module';
+import { RateLimiterGuard } from './rate-limiter/rate-limiter.guard';
+import { RateLimiterModule } from './rate-limiter/rate-limiter.module';
+import { S3Module } from './s3/s3.module';
 
 @Module({
-  imports: [ConfigurationModule, S3Module],
+  imports: [
+    ConfigurationModule,
+    RateLimiterModule,
+    S3Module,
+    ScheduleModule.forRoot(),
+  ],
   controllers: [AppController],
-  providers: [AppService, PrismaService],
+  providers: [AppService, PrismaService, RateLimiterGuard],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(MethodNotAllowedMiddleware).forRoutes('*');
+  }
+}
