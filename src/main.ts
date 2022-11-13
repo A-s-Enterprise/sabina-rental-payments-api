71f0b6063b44e24babe3d8c2604e2d180d15f631
Mark <mac21macky@gmail.com>
2022-11-13 09:20:07 +0800

    Modify 'main' use 'main.config'

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import initMain from './main.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  initMain(app, configService);
+  await app.listen(configService.get('PORT'));
 }
 bootstrap();
