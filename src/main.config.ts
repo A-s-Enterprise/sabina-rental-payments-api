f4f39de65bfdcbfb35fafd2583ae2aa5df1ed6e2
Mark <mac21macky@gmail.com>
2022-11-13 14:49:45 +0800

    Implement swagger api docs

--- src/main.config.ts
+++ src/main.config.ts
@@ -7,6 +7,7 @@ import { useContainer } from 'class-validator';
 import { ConfigService } from '@nestjs/config';
 import helmet from 'helmet';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 export default (app: INestApplication, config: ConfigService) => {
   app.enableCors();
@@ -19,5 +20,14 @@ export default (app: INestApplication, config: ConfigService) => {
   app.useGlobalPipes(
     new ValidationPipe({ transform: true, skipMissingProperties: true }),
   );
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Sabina Rentals Payments API')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('docs', app, document);
+
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
 };
