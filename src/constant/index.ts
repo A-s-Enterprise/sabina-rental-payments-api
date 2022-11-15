b7d03a95e5592bf5d38a7fa8b958e52d025d181b
Mark <mac21macky@gmail.com>
2022-11-15 08:58:52 +0800

    Add new constant

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -4,5 +4,8 @@ const S3_CLIENT = 'S3_CLIENT';
 // Allowed methods for the API
 const METHODS_ALLOWED = ['GET', 'POST', 'PUT', 'DELETE'];
 
-export { S3_CLIENT, METHODS_ALLOWED };
+// Refresh Token Expiration in Seconds
+// 1 week (60 * 60 * 24 * 7) = 604,800
+const REFRESH_TOKEN_EXPIRATION = 604_800;
 
+export { S3_CLIENT, METHODS_ALLOWED, REFRESH_TOKEN_EXPIRATION };
