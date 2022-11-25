dae330d16552f04b6c449de01586841f8454e46a
Mark <mac21macky@gmail.com>
2022-11-25 08:55:50 +0800

    Modify middleware add specific middlewares for 'before' and 'after' actions

---
+++ src/common/prisma/middlewares.ts
@@ -0,0 +1,34 @@
+import { Prisma } from '@prisma/client';
+import { hashPassword, omit } from '../utils';
+import { BadRequestException, HttpException } from '@nestjs/common';
+
+const beforeActionMiddleware: Prisma.Middleware = async (params, next) => {
+  const { model, action, args } = params;
+
+  if (model == 'User' && action == 'create') {
+    params.args.data.password = await hashPassword(args.data.password);
+  }
+
+  const result = await next(params);
+  // the below code executes after `afterActionMiddleware`
+  return result;
+};
+
+const afterActionMiddleware: Prisma.Middleware = async (params, next) => {
+  const { model, action } = params;
+
+  const result = await next(params);
+
+  // the below code executes before
+  // `await next(params)` in `beforeActionMiddleware`
+
+  if (model === 'User' && result) {
+    return Array.isArray(result)
+      ? result.map((val) => omit(val, 'password'))
+      : result;
+  }
+
+  return result;
+};
+
+export { beforeActionMiddleware, afterActionMiddleware };
