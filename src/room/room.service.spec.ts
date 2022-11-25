e2be240705aeea78308d67d64aafb0be4adea385
Mark <mac21macky@gmail.com>
2022-11-25 09:18:47 +0800

    Fix scripts and tests

--- src/room/room.service.spec.ts
+++ src/room/room.service.spec.ts
@@ -1,9 +1,54 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { RoomService } from './room.service';
 import { PrismaService } from './../db/prisma.service';
+import { Room, Status } from '@prisma/client';
+import * as uuid from 'uuid';
 
 describe('RoomService', () => {
   let service: RoomService;
+
+  const rooms: Room[] = [
+    {
+      id: '38c1d40a-230b-4d11-93cc-d32c17c9f6cc',
+      floorId: 'baddb906-4b6e-4041-aff7-ca49e1b2b6cc',
+      roomTypeId: 'ce8b7547-e012-4e40-a8f9-fbe3b8517e7c',
+      name: 'one',
+      tenantOccupancyLimit: 2,
+      status: Status.ACTIVE,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      id: '9cc552aa-98ec-476b-9332-ea7066abbe99',
+      floorId: 'baddb906-4b6e-4041-aff7-ca49e1b2b6cc',
+      roomTypeId: 'ce8b7547-e012-4e40-a8f9-fbe3b8517e7c',
+      name: 'two',
+      tenantOccupancyLimit: 2,
+      status: Status.ACTIVE,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      id: '3aa2974f-423a-4efa-9fdd-497a0f6d59ee',
+      floorId: '0100f0bf-9c51-4d80-a6f9-54bc0d6b8fa5',
+      roomTypeId: 'ce8b7547-e012-4e40-a8f9-fbe3b8517e7c',
+      name: 'three',
+      tenantOccupancyLimit: 3,
+      status: Status.ACTIVE,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      id: '9668ee3e-4a07-4ee6-b9eb-4784ba08a4af',
+      floorId: 'f6d81371-f680-4edf-a2bc-3c8b2cb2b2ee',
+      roomTypeId: '53c2bc7d-a3ca-4a31-8dd3-74d722d12e5a',
+      name: 'four',
+      tenantOccupancyLimit: 3,
+      status: Status.INACTIVE,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+  ];
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -11,7 +56,20 @@ describe('RoomService', () => {
         RoomService,
         {
           provide: PrismaService,
-          useValue: {},
+          useValue: {
+            room: {
+              findMany: jest.fn().mockResolvedValue(rooms),
+              findUnique: jest.fn().mockResolvedValue(rooms[0]),
+              create: jest.fn().mockImplementation(async (data) => {
+                return {
+                  ...data,
+                  id: uuid.v4(),
+                };
+              }),
+              update: jest.fn().mockResolvedValue(rooms[3]),
+              delete: jest.fn().mockResolvedValue(rooms[4]),
+            },
+          },
         },
       ],
     }).compile();
@@ -21,5 +79,54 @@ describe('RoomService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('findMany', () => {
+    it('should retrieve many rooms', async () => {
+      await expect(service.findMany({})).resolves.toEqual(rooms);
+    });
+  });
+
+  describe('findOne', () => {
+    describe('findById', () => {
+      it('should return the `room` base on the id', async () => {
+        await expect(
+          service.findById('38c1d40a-230b-4d11-93cc-d32c17c9f6cc'),
+        ).resolves.toEqual(rooms[0]);
+      });
+    });
+
+    describe('findByName', () => {
+      it('should return the `room` base on the name', async () => {
+        await expect(service.findByName('one')).resolves.toEqual(rooms[0]);
+      });
+    });
+  });
+
+  describe('create', () => {
+    it('should create a `room` with an `id` property', async () => {
+      await expect(
+        service.create({
+          floorId: 'aa',
+          roomTypeId: 'aaa',
+          tenantOccupancyLimit: 2,
+          name: 'five',
+        }),
+      ).resolves.toHaveProperty('id');
+    });
+  });
+
+  describe('updateById', () => {
+    it('should update the room ', async () => {
+      await expect(
+        service.updateById('test-id', { name: 'six' }),
+      ).resolves.toEqual(rooms[3]);
+    });
+  });
+
+  describe('delete', () => {
+    it('should delete the room ', async () => {
+      await expect(service.delete('id')).resolves.toEqual(rooms[4]);
+    });
   });
 });
--- src/room/room.service.ts
+++ src/room/room.service.ts
@@ -1,22 +1,31 @@
-import { Injectable } from '@nestjs/common';
-import { Room } from '@prisma/client';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { Prisma, Room } from '@prisma/client';
 import { PrismaService } from '../db/prisma.service';
 import { CreateRoomDto } from './dto/create-room.dto';
+import { UpdateRoomDto } from './dto/update-room.dto';
 
 @Injectable()
 export class RoomService {
   constructor(private readonly prisma: PrismaService) {}
 
+  findMany(args?: Prisma.RoomFindManyArgs) {
+    return this.prisma.room.findMany(args);
+  }
+
+  findOne(args?: Prisma.RoomFindUniqueArgs) {
+    return this.prisma.room.findUnique(args);
+  }
+
+  findByIdOrThrow(id: string): Promise<Room> {
+    return this.prisma.room.findUniqueOrThrow({ where: { id } });
+  }
+
   findById(id: string): Promise<Room> {
-    return this.prisma.room.findUnique({
-      where: {
-        id,
-      },
-    });
+    return this.prisma.room.findUnique({ where: { id } });
   }
 
   findByName(name: string): Promise<Room> {
-    return this.prisma.room.findFirst({
+    return this.findOne({
       where: {
         name,
       },
@@ -27,5 +36,40 @@ export class RoomService {
     return this.prisma.room.create({
       data,
     });
+  }
+
+  update(
+    where: Prisma.RoomWhereUniqueInput,
+    data: UpdateRoomDto,
+  ): Promise<Room> {
+    return this.prisma.room.update({
+      where,
+      data,
+    });
+  }
+
+  updateById(id: string, data: UpdateRoomDto): Promise<Room> {
+    return this.update({ id }, data);
+  }
+
+  async delete(id: string): Promise<Room> {
+    // can't delete a room if
+    // there's a tenant residing in that room
+    const room = await this.prisma.room.findUnique({
+      where: {
+        id,
+      },
+      include: {
+        _count: {
+          select: { users: true },
+        },
+      },
+    });
+
+    if (room?._count?.users) {
+      throw new BadRequestException('room has existing tenants.');
+    }
+
+    return this.prisma.room.delete({ where: { id } });
   }
 }
