f0c5386661853d74b0922803b929ab8912183ff5
Mark <mac21macky@gmail.com>
2022-11-26 06:49:27 +0800

    Modify implementation for 'IsNameAlreadyExistConstraint' class validator

--- src/common/validators/IsNameAlreadyExist.ts
+++ src/common/validators/IsNameAlreadyExist.ts
@@ -6,37 +6,28 @@ import {
   ValidatorConstraint,
   registerDecorator,
 } from 'class-validator';
-import { FloorService } from '../../floor/floor.service';
-import { RoomTypeService } from '../../room-type/room-type.service';
-import { RoomService } from '../../room/room.service';
+import { PrismaService } from '../../db/prisma.service';
 
 @ValidatorConstraint({ async: true })
 @Injectable()
 export class IsNameAlreadyExistConstraint
   implements ValidatorConstraintInterface
 {
-  constructor(
-    @Optional() private readonly floorService?: FloorService,
-    @Optional() private readonly roomTypeService?: RoomTypeService,
-    @Optional() private readonly roomService?: RoomService,
-  ) {}
+  constructor(private readonly prisma: PrismaService) {}
 
   async validate(value: string, args?: ValidationArguments): Promise<boolean> {
-    const dtoName = args.targetName.toLowerCase();
+    const model = args.targetName
+      .toLowerCase()
+      .replace(/create|update|dto/g, '')
+      .replace('t', 'T');
 
-    if (dtoName.includes('floor') && this.floorService) {
-      return !(await this.floorService.findByName(value));
-    }
+    const result = await this.prisma[model]?.findUnique({
+      where: {
+        name: value,
+      },
+    });
 
-    if (dtoName.includes('room') && this.roomService) {
-      return !(await this.roomService.findByName(value));
-    }
-
-    if (dtoName.includes('roomtype') && this.roomTypeService) {
-      return !(await this.roomTypeService.findByName(value));
-    }
-
-    return false;
+    return !result;
   }
 
   defaultMessage(): string {
