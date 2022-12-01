6be4e7f7d450f8394a94329d417a8f06085a9148
Mark <mac21macky@gmail.com>
2022-12-02 07:53:18 +0800

    Modify guard implementations

--- src/common/guards/admin.guard.ts
+++ src/common/guards/admin.guard.ts
@@ -1,16 +1,18 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { User, UserType } from '@prisma/client';
 import { Request } from 'express';
-import { UserService } from '../../user/user.service';
+import { PrismaService } from '../../db/prisma.service';
 
 @Injectable()
 export class AdminGuard implements CanActivate {
-  constructor(private readonly userService: UserService) {}
+  constructor(private readonly prisma: PrismaService) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const contextHttp = context.switchToHttp();
     const reqUser = contextHttp.getRequest<Request>().user as User;
-    const user = await this.userService.findById(reqUser?.id ?? '');
+    const user = await this.prisma.user.findUnique({
+      where: { id: reqUser?.id ?? '' },
+    });
 
     return user?.type == UserType.ADMIN;
   }
