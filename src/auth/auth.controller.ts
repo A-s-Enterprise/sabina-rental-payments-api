16c1815dacb0ade70dfe9db5173a75b954a2981d
Mark <mac21macky@gmail.com>
2022-12-03 18:39:48 +0800

    Add decorators and remove compose file

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,11 @@
 import { Controller, Post, Body, HttpCode, UseGuards } from '@nestjs/common';
+import {
+  ApiTags,
+  ApiOkResponse,
+  ApiForbiddenResponse,
+  ApiBadRequestResponse,
+  ApiNotFoundResponse,
+} from '@nestjs/swagger';
 import { User } from '../common/decorators/user.decorator';
 import { RefreshTokenGuard } from '../common/guards/refresh-token.guard';
 import { UserTokenData } from '../common/utils';
@@ -6,17 +13,25 @@ import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
 
 @Controller('auth')
+@ApiTags('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('login')
   @HttpCode(200)
+  @ApiOkResponse()
+  @ApiBadRequestResponse({
+    description: 'password provided is incorrect.',
+  })
+  @ApiNotFoundResponse({ description: 'User not found.' })
   login(@Body() dto: LoginDto) {
     return this.authService.login(dto);
   }
 
   @Post('refresh')
   @HttpCode(200)
+  @ApiOkResponse()
+  @ApiForbiddenResponse()
   @UseGuards(RefreshTokenGuard)
   refresh(@User() user: UserTokenData) {
     return this.authService.refresh(user);
